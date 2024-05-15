dd6c9f717b4451c9a7b21320535dd1ca759e6197
Anass <146187264+bokettoo@users.noreply.github.com>
2024-05-15 01:06:20 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,125 +1,59 @@
+document.getElementById("myForm").addEventListener("keydown", FindJoke);
+
+function FindJoke(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        getJoke();
+    }
+}
+
 function getJoke() {
     const amount = document.querySelector("#amount").value;
     const term = document.querySelector("#term").value;
     const xhr = new XMLHttpRequest();
-    if(term && amount === "1" ){
-        const url = `https://v2.jokeapi.dev/joke/Any?type=twopart&contains=${term}&amount=${amount}`;
 
-        xhr.open("GET", url);
+    let url = "https://v2.jokeapi.dev/joke/Any?type=twopart";
+    if (term) url += `&contains=${term}`;
+    if (amount === "1") url += `&amount=${amount}`;
 
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                let joke = JSON.parse(this.responseText);
-                let setup = joke["setup"];
-                let delivery = joke["delivery"];
-                let jokeContainer = document.querySelector("#joke");
-                jokeContainer.innerHTML = `<div class="joke"><p class="setup">${setup}</p><p class="delivery">${delivery}</p></div>`;
-            }
-        } 
-        xhr.send();
-        const form = document.getElementById("myForm");
-        form.reset();
-    } else if(!term && amount === "1" ){
-            const url = `https://v2.jokeapi.dev/joke/Any?type=twopart&amount=${amount}`;
-    
-            xhr.open("GET", url);
-    
-            xhr.onload = function () {
-                if (xhr.status == 200) {
-                    let joke = JSON.parse(this.responseText);
-                    let setup = joke["setup"];
-                    let delivery = joke["delivery"];
-                    let jokeContainer = document.querySelector("#joke");
-                    jokeContainer.innerHTML = `<div class="joke"><p class="setup">${setup}</p><p class="delivery">${delivery}</p></div>`;
+    xhr.open("GET", url);
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            let response = JSON.parse(this.responseText);
+            if (response.hasOwnProperty("setup")) {
+                let setup = response["setup"];
+                let delivery = response["delivery"];
+                if (setup === undefined || delivery === undefined) {
+                    alert("Unable to fetch joke. Please try again later.");
+                } else {
+                    renderJoke(setup, delivery);
                 }
-            } 
-            xhr.send();
-            const form = document.getElementById("myForm");
-            form.reset(); 
-    } else if (amount && term) {
-            const url = `https://v2.jokeapi.dev/joke/Any?type=twopart&contains=${term}&amount=${amount}`;
-        
-            xhr.open("GET", url);
-        
-            xhr.onload = function () {
-                if (xhr.status == 200) {
-                    let response = JSON.parse(this.responseText);
-                    let jokes = response["jokes"];
-                    let jokeContainer = document.querySelector("#joke");
-                    jokeContainer.innerHTML = '';
-                    jokes.forEach(joke => {
-                        let setup = joke["setup"];
-                        let delivery = joke["delivery"];
-                        let jokeElement = document.createElement('div');
-                        jokeElement.classList.add('joke');
-                        jokeElement.innerHTML = `<p class="setup">${setup}</p><p class="delivery">${delivery}</p>`;
-                        jokeContainer.appendChild(jokeElement);
-                    });
-                }
-                const form = document.getElementById("myForm");
-                form.reset();
-            };
-        
-            xhr.send();
-    
-    }   else if(term && !amount ){
-        const url = `https://v2.jokeapi.dev/joke/Any?type=twopart&contains=${term}`;
-
-        xhr.open("GET", url);
-
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                let joke = JSON.parse(this.responseText);
-                let setup = joke["setup"];
-                let delivery = joke["delivery"];
-                let jokeContainer = document.querySelector("#joke");
-                jokeContainer.innerHTML = `<div class="joke"><p class="setup">${setup}</p><p class="delivery">${delivery}</p></div>`;
-            }
-        } 
-        xhr.send();
-        const form = document.getElementById("myForm");
-        form.reset();
-    }   else if (amount && !term) {
-            const url = `https://v2.jokeapi.dev/joke/Any?type=twopart&amount=${amount}`;
-        
-            xhr.open("GET", url);
-        
-            xhr.onload = function () {
-                if (xhr.status == 200) {
-                    let response = JSON.parse(this.responseText);
-                    let jokes = response["jokes"];
-                    let jokeContainer = document.querySelector("#joke");
-                    jokeContainer.innerHTML = '';
-                    jokes.forEach(joke => {
-                        let setup = joke["setup"];
-                        let delivery = joke["delivery"];
-                        let jokeElement = document.createElement('div');
-                        jokeElement.classList.add('joke');
-                        jokeElement.innerHTML = `<p class="setup">${setup}</p><p class="delivery">${delivery}</p>`;
-                        jokeContainer.appendChild(jokeElement);
-                    });
-                }
-                const form = document.getElementById("myForm");
-                form.reset();
-            };
-        
-            xhr.send();
-    
-    }  else if (!term && !amount) {
-        const url = "https://v2.jokeapi.dev/joke/Any?type=twopart";
-        xhr.open("GET", url);
-
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                let joke = JSON.parse(this.responseText);
-                let setup = joke["setup"];
-                let delivery = joke["delivery"];
-                let jokeContainer = document.querySelector("#joke");
-                jokeContainer.innerHTML = `<div class="joke"><p class="setup">${setup}</p><p class="delivery">${delivery}</p></div>`;
+            } else if (response.hasOwnProperty("jokes")) {
+                renderMultipleJokes(response["jokes"]);
             }
         }
-        xhr.send();
         const form = document.getElementById("myForm");
         form.reset();
-    }
+    };
+    xhr.send();
+}
+
+function renderJoke(setup, delivery) {
+    let jokeContainer = document.querySelector("#joke");
+    jokeContainer.innerHTML = `<div class="joke"><p class="setup">${setup}</p><p class="delivery">${delivery}</p></div>`;
+}
+
+function renderMultipleJokes(jokes) {
+    let jokeContainer = document.querySelector("#joke");
+    jokeContainer.innerHTML = '';
+    jokes.forEach(joke => {
+        let setup = joke["setup"];
+        let delivery = joke["delivery"];
+        if (setup !== undefined && delivery !== undefined) {
+            let jokeElement = document.createElement('div');
+            jokeElement.classList.add('joke');
+            jokeElement.innerHTML = `<p class="setup">${setup}</p><p class="delivery">${delivery}</p>`;
+            jokeContainer.appendChild(jokeElement);
+        }
+    });
 }
